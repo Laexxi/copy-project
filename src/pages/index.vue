c0b24ac9e4345e9671f1aaa33e01fb0f08ef4d59
Laexxi <evoscith@gmail.com>
2024-01-03 13:55:41 +0100

    Localization

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,7 +2,7 @@
   <v-container class="fill-height">
     <v-row justify="center" align="center">
       <v-col cols="12" class="text-center">
-        <h1 class="text-h4 py-4 mt-10">Start Analysis</h1>
+        <h1 class="text-h4 py-4 mt-10">{{ translate('index.title') }}</h1>
         <v-btn color="primary" class="mt-10" @click="" variant="elevated" height="200" width="400" elevation="10"
           rounded="lg">
           <v-icon icon="mdi-folder-search" size="50"></v-icon>
@@ -12,15 +12,10 @@
   </v-container>
 </template>
   
-<script>
-export default {
-  methods: {
-    startAnalysis() {
-      // Logik zum Starten der Analyse
-      console.log("Starting analysis...");
-    }
-  }
-};
+<script setup>
+import { useInternationalization } from '../hooks/useInternationalization.js';
+const { translate } = useInternationalization();
+
 </script>
   
 <style scoped></style>
